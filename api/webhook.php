fdf8ee1b94c3a8220342fb87e48e61e9805c23b3
xiaoyi510 <loveyybin@qq.com>
2021-06-16 14:56:26 +0800

    修改方式

--- api/webhook.php
+++ api/webhook.php
@@ -5,7 +5,7 @@ $bot = new Bot();
 // 获取/token 命令，获取聊天 id
 $data = json_decode(file_get_contents("php://input"), true);
 $message = $data['message']['text'] ?? '';
-if ($message === '/token') {
+if ($message === '/start') {
     $chat_id = $data['message']['chat']['id'];
 
     $bot->sendMessage(['text' => '请求网址即可发送消息:
